5a7f9350e3cbf862c4dace71c575e5e5578e3ed7
AIT SIDI HOU Mohamed <aitsidihoum@gmail.com>
2025-04-15 18:35:22 +0200

    feat(linear_reg): import in binding

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
 use pyo3::prelude::*;
+mod activation_function;
+mod data_converter;
+mod data_manager;
+mod loss;
+mod matrix;
+mod mlp;
 
-/// Formats the sum of two numbers as string.
+mod linear_regression;
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn predict_mlp(input: Vec<Vec<f32>>) -> PyResult<i32> {
+    Ok(mlp::predict(input))
 }
+
+#[pyfunction]
+fn train_mlp(
+    dataset: Vec<Vec<Vec<Vec<f32>>>>,
+    nb_epoch: i32,
+    hidden_layers: Vec<i32>,
+    training_name: String
+) -> PyResult<()> {
+    Ok(mlp::training(dataset, nb_epoch, hidden_layers, training_name))
+}
+
+
+#[pyfunction]
+fn train_linear_regression(
+    x_data : Vec<f32>,
+    y_data : Vec<f32>,
+    verbose : bool,
+    epochs: i32,
+    training_name: String
+) -> PyResult<()> {
+    Ok(linear_regression::train(x_data, y_data, verbose, epochs, training_name))
+}
+
+
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn better_tensorflow(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(predict_mlp, m)?)?;
+    m.add_function(wrap_pyfunction!(train_mlp, m)?)?;
+    m.add_function(wrap_pyfunction!(train_linear_regression, m)?)?;
     Ok(())
 }
