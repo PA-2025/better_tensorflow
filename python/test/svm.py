d8bbf9819b53e7f581d012daa4ca860b1f4a0340
victordalet <victordalet@protonmail.com>
2025-07-17 16:15:43 +0200

    fix(svm): remove bad argumnt

--- python/test/svm.py
+++ python/test/svm.py
@@ -12,7 +12,7 @@ y_xor = [-1, 1, 1, -1]  # Sortie XOR encodée pour SVM
 X_test = X_xor
 
 # === SVM RBF ===
-svm_rbf = KernelSVM("rbf", 2.0, lr=0.1, lambda_svm=0.01, epochs=200)
+svm_rbf = KernelSVM("rbf", 2.0, lr=0.1, lambda_svm=0.01)
 print("=== SVM avec noyau RBF ===")
 svm_rbf.fit(X_xor, y_xor, path="rbf_xor.weights")
 print("=== Prédictions RBF ===")
@@ -22,7 +22,7 @@ for x, pred in zip(X_test, preds_rbf):
     print(f"Entrée: {x} => Prédiction RBF: {pred}")
 
 # === SVM Polynomial ===
-svm_poly = KernelSVM("poly", 2, lr=0.1, lambda_svm=0.01, epochs=200)
+svm_poly = KernelSVM("poly", 2, lr=0.1, lambda_svm=0.01)
 print("=== SVM avec noyau Polynomial ===")
 svm_poly.fit(X_xor, y_xor, path="poly_xor.weights")
 print("=== Prédictions Polynomial ===")
